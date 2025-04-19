06deb44ecaa1f8914cc1ed5480266c77b642b05c
Nguyễn Thế Hưng <42248228+teppyboy@users.noreply.github.com>
2025-04-19 13:30:41 +0000

    feat: gh action

--- ubuntu-windows/build.sh
+++ ubuntu-windows/build.sh
@@ -10,4 +10,6 @@ echo "Setting the brand to Twilight for packaging..."
 # We only set the brand here because we don't want to include official Zen update tracking and etc.
 npm run surfer -- ci --brand twilight
 echo "Packaging Zen Browser..."
+# We set +e because "npm run package" will "fail" but we still have the archive anyway
+set +e
 npm run package
