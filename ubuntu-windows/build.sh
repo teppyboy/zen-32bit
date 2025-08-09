bf0fcf15b1d5c76ae4dc8d4288f5556d7c4774f9
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-08-09 18:38:14 +0700

    chore: branding before build
    Idc anymore, it is Zen and it should look like Zen :D

--- ubuntu-windows/build.sh
+++ ubuntu-windows/build.sh
@@ -2,13 +2,12 @@
 
 set -e
 source ./ubuntu-windows/env.sh
+echo "Setting the brand to Twilight for packaging..."
+npm run surfer -- ci --brand twilight
 echo "Building Zen Browser..."
 echo "If this fails then you may try 'npm run build -- --verbose' to see more output."
 cd ./zen-browser/desktop
 npm run build
-echo "Setting the brand to Twilight for packaging..."
-# We only set the brand here because we don't want to include official Zen update tracking and etc.
-npm run surfer -- ci --brand twilight
 echo "Packaging Zen Browser..."
 # We set || true because "npm run package" will "fail" but we still have the archive anyway
 npm run package || true
