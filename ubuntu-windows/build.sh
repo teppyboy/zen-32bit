f9e75ba1dd071bda8cca54a89a229d31b88b9f1d
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-08-10 12:14:54 +0700

    fix: cd first
    I forgot

--- ubuntu-windows/build.sh
+++ ubuntu-windows/build.sh
@@ -2,11 +2,11 @@
 
 set -e
 source ./ubuntu-windows/env.sh
+cd ./zen-browser/desktop
 echo "Setting the brand to Twilight for packaging..."
-npm run surfer -- ci --brand twilight
+npm run surfer -- ci --brand twilight || true
 echo "Building Zen Browser..."
 echo "If this fails then you may try 'npm run build -- --verbose' to see more output."
-cd ./zen-browser/desktop
 npm run build
 echo "Packaging Zen Browser..."
 # We set || true because "npm run package" will "fail" but we still have the archive anyway
